d1cac35f917ce09078d3a40746c2dba6ac07256d
Oleksandr Tkach <sasha2003tkach001100@gmail.com>
2024-02-10 16:46:27 +0200

    added liner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from "@shared/shared.module";
-import {CoreModule} from "@core/core.module";
+import { SharedModule } from '@shared/shared.module';
+import { CoreModule } from '@core/core.module';
 import { AuthModule } from '@modules/auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    CoreModule,
-    AuthModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, CoreModule, AuthModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
