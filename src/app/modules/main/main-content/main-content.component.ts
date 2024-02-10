526e335d1ae1af0eb85e199ebca2b4677e04946a
lordoleksiy <lordoleksiy@gmail.com>
2024-02-10 22:41:32 +0200

    feature: add visual for main page

--- src/app/modules/main/main-content/main-content.component.ts
+++ src/app/modules/main/main-content/main-content.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IProduct} from "../../../models/IProduct";
+import {IProduct, ProductStatus} from "../../../models/IProduct";
 
 @Component({
     selector: 'app-main-content',
@@ -9,4 +9,51 @@ import {IProduct} from "../../../models/IProduct";
 export class MainContentComponent {
   products: IProduct[]  = []
 
+  pageSize = 12;
+  currentPage = 1;
+
+  getCurrentPageProducts() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    return this.products.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.getTotalPages()) {
+      this.currentPage++;
+    }
+  }
+
+  getTotalPages() {
+    return Math.ceil(this.products.length / this.pageSize);
+  }
+  constructor() {
+    this.populateTestData();
+  }
+
+  private populateTestData() {
+    for (let i = 1; i <= 50; i++) {
+      const product: IProduct = {
+        title: `Product ${i}`,
+        description: `Description for Product ${i}`,
+        price: 10 * i,
+        minimalBid: 50 * i,
+        phone: `123-456-${i}`,
+        imageLinks: `https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?rs=1&pid=ImgDetMain`,
+        status: ProductStatus.Active,
+        endDate: new Date(`2022-12-${i}`),
+        sellerEmail: `seller${i}@example.com`,
+        winnerEmail: i % 2 === 0 ? `winner${i}@example.com` : null,
+      };
+
+      this.products.push(product);
+    }
+  }
+
+  protected readonly ProductStatus = ProductStatus;
 }
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -8,10 +8,11 @@ import {MatOptionModule} from "@angular/material/core";
 import {MatSelectModule} from "@angular/material/select";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatInputModule} from "@angular/material/input";
-import {NgForOf} from "@angular/common";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
     declarations: [MainComponent, MainContentComponent],
@@ -26,7 +27,10 @@ import {MatExpansionModule} from "@angular/material/expansion";
     NgForOf,
     MatPaginatorModule,
     FlexLayoutModule,
-    MatExpansionModule
+    MatExpansionModule,
+    DatePipe,
+    NgIf,
+    MatButtonModule
   ],
 })
 export class MainModule {}
