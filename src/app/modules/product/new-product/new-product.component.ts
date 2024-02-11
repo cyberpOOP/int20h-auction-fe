eba81a3abb75fe845ba890d72262007720cff1e0
double-hope <nadia.prohorchuk@gmail.com>
2024-02-11 16:55:41 +0200

    small fixes

---
+++ src/app/modules/product/new-product/new-product.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-new-product',
+    templateUrl: './new-product.component.html',
+    styleUrls: ['./new-product.component.scss'],
+})
+export class NewProductComponent {}
